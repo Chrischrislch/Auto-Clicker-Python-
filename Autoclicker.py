698f6802bebb6ac276aa5f69f4bf6fafa531c69d
chrichrischris <74584754+Chrischrislch@users.noreply.github.com>
2022-01-04 21:34:30 +0800

    Update Autoclicker.py

--- Autoclicker.py
+++ Autoclicker.py
@@ -10,13 +10,13 @@ if (t < 0):
     sys.exit()
 elif (t == 0):
 	print('infinite!')
-d = float(input('Interval of each click (s)：'))
+d = float(input('Interval(s) of each click (can be 0)：'))
 p = int(input('click?（1=left，2=right，3=middle）：'))
 print('You have 5 seconds to move the cursor!')
 for i in range(5):
 	print('Coming',5-i,'S')
 	time.sleep(1)
-print('[I]Start to click.')
+print('Start to click.')
 s = time.time()
 i = 0
 while(i < t or t == 0):
